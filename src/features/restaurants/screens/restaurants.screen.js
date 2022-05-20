15b0e8cf09151d37c31291861e2d2b4262a0a215
Pragasen Naicker <pragasen@noldor.co.za>
2022-05-20 12:52:44 +0200

    restaurant card completed

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight}px && margin-top: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,14 @@
+import { colors } from "./colors";
+import { sizes } from "./sizes";
+import { lineHeights, space } from "./spacing";
+import { fonts, fontWeights, fontSizes } from "./font";
+
+export const theme = {
+  colors,
+  sizes,
+  lineHeights,
+  space,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
