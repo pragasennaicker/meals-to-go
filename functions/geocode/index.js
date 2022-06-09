9944afbec7f71c6f54b91133572b7a1b45f82823
Pragasen Naicker <pnaicker.dev@gmail.com>
2022-06-09 14:46:35 +0200

    google api integration

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,27 @@
 const { locations: locationsMock } = require("./geocode.mock");
+const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (req, res) => {
-  const { city } = req.query;
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = request.query;
 
-  res.json(locationMock);
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().goole.key,
+      },
+      timeout: 1000,
+    })
+    .then((result) => {
+      return response.json(result.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -11,9 +15,9 @@ const { placesRequest } = require("./places");
 // });
 
 exports.geocode = functions.https.onRequest((req, res) => {
-  geocodeRequest(req, res);
+  geocodeRequest(req, res, client);
 });
 
 exports.placesNearby = functions.https.onRequest((req, res) => {
-  placesRequest(req, res);
+  placesRequest(req, res, client);
 });
